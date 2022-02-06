12c791319140157cfc3a82c0600cb054e5ab48a6
zyy <1113710198@qq.com>
2022-02-07 02:04:02 +0800

    init repo

---
+++ src/test/java/com/zyy/xue/demos/DemosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zyy.xue.demos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
